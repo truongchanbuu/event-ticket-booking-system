d9f645732e552ecb6289a5b65128318dedefd3d1
LAPTOP-2BL5PO8I\buutruong <truongchanbuu0512@gmail.com>
2025-06-01 20:47:49 +0700

    chore: setup logger & error handler

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,11 +1,25 @@
 import express from 'express';
+import { scopePerRequest } from 'awilix-express';
+
+import healthRouter from './routes/health.js';
+import AppError from '../../shared/error/app_error.js';
+import { errorHandler } from '../../shared/error/error_handler.js';
+import error_code from '../../shared/error/error_code.js';
 
 export async function createApp() {
   const app = express();
 
   app.use(express.json());
+  app.use(scopePerRequest(container));
 
   // Apply routes
+  // app.use(...)
+  app.use('/health', healthRouter);
+  app.use((req, res, next) => {
+    next(new AppError('Not Found', 404, error_code.NOT_FOUND));
+  });
+
+  app.use(errorHandler);
 
   return app;
 }
