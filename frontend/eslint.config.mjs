712c7a1213f6563ff8c86ec10fac6ede58b501a8
LAPTOP-2BL5PO8I\buutruong <truongchanbuu0512@gmail.com>
2025-06-04 19:19:51 +0700

    feat(frontend): added eslint rule + tailwindcss eslint plugin + added prettier setup + tailwindcss plugin + sort imports plugin

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,16 +1,36 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:prettier/recommended"
+  ),
+  {
+    rules: {
+      "no-console": ["error", { allow: ["info", "warn", "error"] }],
+      "tailwindcss/classnames-order": "error",
+      "tailwindcss/no-custom-classname": "warn",
+      "prettier/prettier": "error",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+    },
+    overrides: [
+      {
+        files: ["*.ts", "*.tsx", "*.js"],
+        parser: "@typescript-eslint/parser",
+      },
+    ],
+  },
+]
 
-export default eslintConfig;
+export default eslintConfig
