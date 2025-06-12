42f714052c6c50ba88f2d4aa3160fe5a85123ce4
LAPTOP-2BL5PO8I\buutruong <truongchanbuu0512@gmail.com>
2025-06-12 10:59:15 +0700

    feat(shared): add notification type enum

--- shared/index.js
+++ shared/index.js
@@ -6,8 +6,12 @@ export {
   FirebaseAuthErrorMap,
   fromFirebaseAuthError,
 } from "./error/index.js";
+
 export { createLogger } from "./logger/index.js";
+
 export { default as BaseValidator } from "./validator/base_validator.js";
 export { default as ROLE } from "./enums/role.enum.js";
+export { default as NOTIFICATION } from "./enums/notification.enum.js";
 export { default as EnumHelper } from "./enums/helpers/enum.helper.js";
+
 export { default as checkJson } from "./validator/helpers/syntax.validator.js";
