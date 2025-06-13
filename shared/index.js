e3a21dacf3295b10fb949808eb4b0c1af9ffa498
LAPTOP-2BL5PO8I\buutruong <truongchanbuu0512@gmail.com>
2025-06-13 15:12:41 +0700

    fix(shared): fix kafka import

--- shared/index.js
+++ shared/index.js
@@ -9,7 +9,7 @@ export {
 
 export { createLogger } from "./logger/index.js";
 
-export * as KafkaUtils from "./kafka";
+export * as KafkaUtils from "./kafka/index.js";
 
 export { default as BaseValidator } from "./validator/base_validator.js";
 export { default as ROLE } from "./enums/role.enum.js";
